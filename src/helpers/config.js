9512fde20a2a005210ee2c3a42b11ba5a16d997d
Moises-Drum <moises.drumand18@gmail.com>
2024-09-12 19:37:26 -0400

    Adicionado descrição sobre a API_URL.

--- src/helpers/config.js
+++ src/helpers/config.js
@@ -1,2 +1,3 @@
 
-export const API_URL = 'Adicione a url e a porta configurada no laravel (http://0.0.0.0:0000)';
+export const API_URL = 'Adicione a url e a porta configurada no laravel (http://0.0.0.0:0000).'
+'OBS. IP nativo do laravel (127.0.0.1) não funciona. Use o da maquina que está executando o servidor.'
